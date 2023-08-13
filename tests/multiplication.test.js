fe77546c72db961ec90d8fd214be5c1701899f5f
jogna <jordan.gnamien@smile.ci>
2023-08-13 23:35:01 +0000

    secoond-auto-test

--- tests/multiplication.test.js
+++ tests/multiplication.test.js
@@ -1,4 +1,4 @@
-const multiplication = require('./multiplication');
+const multiplication = require('./code/multiplication');
 
 test('multiplication 1 * 2 to equal 2', () => {
     expect(multiplication(1,2)).toBe(2);
